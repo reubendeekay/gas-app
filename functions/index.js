35f53ffd8063dbd20ad2885cabcf7a65b2a1ae93
Reuben Jefwa <mrreubenyt@gmail.com>
2022-07-06 15:50:14 +0300

    Transferred Firebase Config

--- functions/index.js
+++ functions/index.js
@@ -1,203 +1,171 @@
-
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const axios = require("axios");
 
-
-const parse = require("./parse");
+const parse = require("../parse");
 
 admin.initializeApp();
 
 exports.lmno_callback_url = functions.https.onRequest(async (req, res) => {
-    const callbackData = req.body.Body.stkCallback;
-    const parsedData = parse(callbackData);
+  const callbackData = req.body.Body.stkCallback;
+  const parsedData = parse(callbackData);
 
-    let uid = req.query.uid.split("/")[0];
-   let amount = req.query.uid.split("/")[1];
-   let providerUid = req.query.uid.split("/")[2];
-   
-    
+  let uid = req.query.uid.split("/")[0];
+  let amount = req.query.uid.split("/")[1];
+  let providerUid = req.query.uid.split("/")[2];
 
-    
-    let transaction = admin.firestore().collection('mpesa').doc('transaction').collection(uid).doc('/' + parsedData.checkoutRequestID + '/');
-    let wallet = admin.firestore().collection('providers').doc(providerUid).collection('account').doc('analytics');
+  let transaction = admin
+    .firestore()
+    .collection("mpesa")
+    .doc("transaction")
+    .collection(uid)
+    .doc("/" + parsedData.checkoutRequestID + "/");
+  let wallet = admin
+    .firestore()
+    .collection("providers")
+    .doc(providerUid)
+    .collection("account")
+    .doc("analytics");
 
-   
-   
-
-   
-    console.log('TYPE OF AMOUNT: ' + typeof parsedData.amount);
-    if ((await transaction.get()).exists){
-    
-}else {
-    if ((typeof parsedData.amount) === (typeof 1)) {
-    
-   
-        
-        let balance = await wallet.get().then(value => value.data().balance);
-        await wallet.update({
-            'balance': parsedData.amount + balance
-        });
-        console.log(parsedData.amount);
-    
-    
-    
-  
-}
-
-    }
-    
-
-    
-await admin.firestore().collection('mpesa').doc('all').collection('users').doc().set({
-    'amount': amount,
-    'type': 'deposit',
-    'userId': uid,
-    'phoneNumber': parsedData.phoneNumber,
-'date': parsedData.transactionDate,
-});
-
-    
-    
+  console.log("TYPE OF AMOUNT: " + typeof parsedData.amount);
   if ((await transaction.get()).exists) {
-    await transaction.update(parsedData);
-} else {
-    await transaction.set(parsedData);
-} 
+  } else {
+    if (typeof parsedData.amount === typeof 1) {
+      let balance = await wallet.get().then((value) => value.data().balance);
+      await wallet.update({
+        balance: parsedData.amount + balance,
+      });
+      console.log(parsedData.amount);
+    }
+  }
 
-   
-
-
-
-res.status(200).send('OK');
-
-});
-
-
-
-exports.notifocations = functions.firestore
-    .document('userData/{userId}/notifications/{notification}')
-    .onCreate(async (snap, context) => {
-        console.log('----------------start function--------------------')
-
-        const doc = snap.data()
-        console.log(doc)
-        const message=doc.message;
-        const uid = context.params.userId;
-        
-
-
-
-
-        admin
-        .firestore()
-        .collection('users')
-        .where('userId', '==', uid)
-        .get()
-        .then(querySnapshot => {
-          querySnapshot.forEach(userTo => {
-            console.log(`Found user to: ${userTo.data().fullName}`)
-           
-            // Get info user from (sent)
-          
-                  const payload = {
-  
-                    notification: {
-                      id:'/notifications/'+doc.id,
-                      title: 'AutoConnect',
-                      body: message,
-                      badge: '1',
-                      sound: 'default'
-                    },
-                    data: {
-                      profilePic: userTo.data().profilePic,
-                      
-                    }
-                  }
-                  // Let push to the target device
-                  admin
-                    .messaging()
-                    .sendToDevice(userTo.data().pushToken, payload)
-                    .then(response => {
-                      console.log('Successfully sent message:', response)
-                    })
-                    .catch(error => {
-                      console.log('Error sending message:', error)
-                    })
-                })
-              })
-          
-          
-          
-        
-      return null
+  await admin
+    .firestore()
+    .collection("mpesa")
+    .doc("all")
+    .collection("users")
+    .doc()
+    .set({
+      amount: amount,
+      type: "deposit",
+      userId: uid,
+      phoneNumber: parsedData.phoneNumber,
+      date: parsedData.transactionDate,
     });
 
+  if ((await transaction.get()).exists) {
+    await transaction.update(parsedData);
+  } else {
+    await transaction.set(parsedData);
+  }
 
+  res.status(200).send("OK");
+});
 
- 
-exports.sendNotification = functions.firestore
-  .document('chats/{chatRoom}/messages/{message}')
+exports.notifocations = functions.firestore
+  .document("userData/{userId}/notifications/{notification}")
   .onCreate(async (snap, context) => {
-    console.log('----------------start function--------------------')
+    console.log("----------------start function--------------------");
 
-    const doc = snap.data()
-    console.log(doc)
+    const doc = snap.data();
+    console.log(doc);
+    const message = doc.message;
+    const uid = context.params.userId;
 
-    const idFrom = doc.sender
-    const idTo = doc.to
-    const contentMessage = doc.message
+    admin
+      .firestore()
+      .collection("users")
+      .where("userId", "==", uid)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((userTo) => {
+          console.log(`Found user to: ${userTo.data().fullName}`);
 
+          // Get info user from (sent)
+
+          const payload = {
+            notification: {
+              id: "/notifications/" + doc.id,
+              title: "AutoConnect",
+              body: message,
+              badge: "1",
+              sound: "default",
+            },
+            data: {
+              profilePic: userTo.data().profilePic,
+            },
+          };
+          // Let push to the target device
+          admin
+            .messaging()
+            .sendToDevice(userTo.data().pushToken, payload)
+            .then((response) => {
+              console.log("Successfully sent message:", response);
+            })
+            .catch((error) => {
+              console.log("Error sending message:", error);
+            });
+        });
+      });
+
+    return null;
+  });
+
+exports.sendNotification = functions.firestore
+  .document("chats/{chatRoom}/messages/{message}")
+  .onCreate(async (snap, context) => {
+    console.log("----------------start function--------------------");
+
+    const doc = snap.data();
+    console.log(doc);
+
+    const idFrom = doc.sender;
+    const idTo = doc.to;
+    const contentMessage = doc.message;
 
     // Get push token user to (receive)
     admin
       .firestore()
-      .collection('users')
-      .where('userId', '==', idTo)
+      .collection("users")
+      .where("userId", "==", idTo)
       .get()
-      .then(querySnapshot => {
-        querySnapshot.forEach(userTo => {
-          console.log(`Found user to: ${userTo.data().fullName}`)
-         
+      .then((querySnapshot) => {
+        querySnapshot.forEach((userTo) => {
+          console.log(`Found user to: ${userTo.data().fullName}`);
+
           // Get info user from (sent)
           admin
             .firestore()
-            .collection('users')
-            .where('userId', '==', idFrom)
+            .collection("users")
+            .where("userId", "==", idFrom)
             .get()
-            .then(querySnapshot2 => {
-              querySnapshot2.forEach(userFrom => {
-                console.log(`${userFrom.data().fullName}`)
+            .then((querySnapshot2) => {
+              querySnapshot2.forEach((userFrom) => {
+                console.log(`${userFrom.data().fullName}`);
                 const payload = {
-
                   notification: {
-                    id:'/chat-screen',
+                    id: "/chat-screen",
                     title: `${userFrom.data().fullName}`,
                     body: contentMessage,
-                    badge: '1',
-                    sound: 'default'
+                    badge: "1",
+                    sound: "default",
                   },
                   data: {
                     profilePic: userFrom.data().profilePic,
-                    
-                  }
-                }
+                  },
+                };
                 // Let push to the target device
                 admin
                   .messaging()
                   .sendToDevice(userTo.data().pushToken, payload)
-                  .then(response => {
-                    console.log('Successfully sent message:', response)
+                  .then((response) => {
+                    console.log("Successfully sent message:", response);
                   })
-                  .catch(error => {
-                    console.log('Error sending message:', error)
-                  })
-              })
-            })
-        })
-        
-        
-      })
-    return null
-  })
-
+                  .catch((error) => {
+                    console.log("Error sending message:", error);
+                  });
+              });
+            });
+        });
+      });
+    return null;
+  });
